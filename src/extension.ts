63ed62fe34334394a93f895df1409eadf4110300
Adam Crossland <Adam Crossland>
2017-01-31 22:21:10 -0500

    Added check for no open workspace.

--- src/extension.ts
+++ src/extension.ts
@@ -3,15 +3,14 @@ import * as path from 'path';
 
 function createIQManifestFile() {
     let newFile = null;
-    try {
-        newFile = vscode.Uri.parse('untitled:' + path.join(vscode.workspace.rootPath, 'manifest.xml'));
-    } catch (e) {
-        console.error(e);
-    }
-    vscode.workspace.openTextDocument(newFile).then(document => {
-    const edit = new vscode.WorkspaceEdit();
-    edit.insert(newFile, new vscode.Position(0, 0),
-    `<iq:manifest version="1">
+
+    if (vscode.workspace.rootPath) {
+        newFile = vscode.Uri.parse('untitled:' + path.join(vscode.workspace.rootPath, 'manifest.xml'))
+
+        vscode.workspace.openTextDocument(newFile).then(document => {
+            const edit = new vscode.WorkspaceEdit();
+            edit.insert(newFile, new vscode.Position(0, 0),
+                `<iq:manifest version="1">
     <iq:application entry="" id="" name="" launcherIcon="" type="" minSdkVersion="">
         <iq:products>
         </iq:products>
@@ -20,14 +19,17 @@ function createIQManifestFile() {
     </iq:application>
  </iq:manifest>`);
 
-    return vscode.workspace.applyEdit(edit).then(success => {
-        if (success) {
-            vscode.window.showTextDocument(document);
-        } else {
-            vscode.window.showInformationMessage('Could not create Manifest file.');
-        }
-    });
-});
+            return vscode.workspace.applyEdit(edit).then(success => {
+                if (success) {
+                    vscode.window.showTextDocument(document);
+                } else {
+                    vscode.window.showInformationMessage('Could not create Connect IQ Manifest file.');
+                }
+            });
+        });
+    } else {
+        vscode.window.showErrorMessage("You must open a folder before creating a new Connect IQ project.");
+    }
 }
 
 export function activate(context: vscode.ExtensionContext) {
