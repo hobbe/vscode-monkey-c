4e40b07e95f05eceb30724660b58c0ea694a9a9b
Adam Crossland <Adam Crossland>
2017-01-31 22:06:28 -0500

    Added initial manifest creation.

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,38 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
+
+function createIQManifestFile() {
+    let newFile = null;
+    try {
+        newFile = vscode.Uri.parse('untitled:' + path.join(vscode.workspace.rootPath, 'manifest.xml'));
+    } catch (e) {
+        console.error(e);
+    }
+    vscode.workspace.openTextDocument(newFile).then(document => {
+    const edit = new vscode.WorkspaceEdit();
+    edit.insert(newFile, new vscode.Position(0, 0),
+    `<iq:manifest version="1">
+    <iq:application entry="" id="" name="" launcherIcon="" type="" minSdkVersion="">
+        <iq:products>
+        </iq:products>
+        <iq:permissions>
+        </iq:permission>
+    </iq:application>
+ </iq:manifest>`);
+
+    return vscode.workspace.applyEdit(edit).then(success => {
+        if (success) {
+            vscode.window.showTextDocument(document);
+        } else {
+            vscode.window.showInformationMessage('Could not create Manifest file.');
+        }
+    });
+});
+}
 
 export function activate(context: vscode.ExtensionContext) {
     var disposable = vscode.commands.registerCommand('extension.newiqproject', () => {
-        vscode.window.showInformationMessage('This action will create a new Connect IQ Manifest file.');
+        createIQManifestFile();
     });
 
     context.subscriptions.push(disposable);
